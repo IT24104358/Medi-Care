e47e9ddc39f51c0370e3cb1ceedb8f59058f1aae
IT24102618 <it24102618@my.sliit.lk>
2025-05-19 08:16:46 +0530

    feat:update

--- src/main/java/com/medicare/medicare/model/User.java
+++ src/main/java/com/medicare/medicare/model/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true) // This tells Jackson to ignore unknown properties
@@ -18,6 +18,62 @@ public class User {
     private String address;
     private String password;
     private String role = "USER"; // Default role is USER
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
     // Mark this as JsonProperty so it's properly handled in serialization/deserialization
     @JsonProperty("admin")
